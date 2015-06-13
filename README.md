71b2e17118b8848a5c5d6d7d5b6ea9a9495a03c0
Wim <wim@42.be>
2015-06-13 21:10:55 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # gomphs
+
+Simple tool to ping multiple hosts at once with an overview
+
+## usage 
+
+```
+# gomphs
+usage:
+  -hosts="": ip addresses to ping, space seperated (e.g "8.8.8.8 8.8.4.4")
+  -showrtt=false: show roundtrip time in ms
+```
+
+. host is up
+! host is down
+
+* With showrtt enabled 
+
+When rtt > 1s it will just show ">1s"  
+When rtt < 0ms it will just show 0  
+
+After 20 pings the header will be repeated
+
+## example
+
+```
+# gomphs -hosts "8.8.8.8 192.168.1.1 8.8.4.4"
+	| 1 | 2 | 3 |
+0000| . | ! | . |
+0001| . | ! | . |
+0002| . | ! | . |
+0003| . | ! | . |
+0004| . | ! | . |
+0005| . | ! | . |
+```
+
+## example
+
+```
+# gomphs -hosts "8.8.8.8 192.168.1.1 8.8.4.4" -showrtt
+    |   1 |   2 |   3 |
+0000|  13 |   ! |  12 |
+0001|  12 |   ! |  12 |
+0002|  11 |   ! |  13 |
+0003|  11 |   ! |  13 |
+0004|  12 |   ! |  12 |
+0005|  12 |   ! |  12 |
+```
---
+++ gomphs.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/tatsushid/go-fastping"
+)
+
+var pingIP string
+var showRTT bool
+var width string = "2"
+
+func init() {
+	flag.StringVar(&pingIP, "hosts", "", "ip addresses to ping, space seperated (e.g \"8.8.8.8 8.8.4.4\")")
+	flag.BoolVar(&showRTT, "showrtt", false, "show roundtrip time in ms")
+	flag.Parse()
+	if flag.NFlag() == 0 {
+		fmt.Println("usage: ")
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
+	if showRTT {
+		width = "4"
+	}
+}
+
+type MilliDuration time.Duration
+
+func (hd MilliDuration) String() string {
+	milliseconds := time.Duration(hd).Nanoseconds()
+	milliseconds = milliseconds / 1000000
+	if milliseconds > 1000 {
+		return fmt.Sprintf(">1s")
+	} else {
+		return fmt.Sprintf("%4d", milliseconds)
+	}
+}
+
+func printHeader() {
+	fmt.Printf("   ")
+	for index, _ := range strings.Fields(pingIP) {
+		fmt.Printf(" |%"+width+"d", index+1)
+	}
+	fmt.Println(" |")
+}
+
+func main() {
+	var mylist []string
+	var rowcounter int = 0
+	result := make(map[string]string)
+	p := fastping.NewPinger()
+
+	for _, host := range strings.Fields(pingIP) {
+		ra, _ := net.ResolveIPAddr("ip4:icmp", host)
+		p.AddIPAddr(ra)
+		mylist = append(mylist, host)
+	}
+	p.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
+		result[addr.String()] = MilliDuration(rtt).String()
+	}
+	p.OnIdle = func() {
+		if rowcounter%20 == 0 {
+			printHeader()
+			if rowcounter == 10000 {
+				rowcounter = 0
+			}
+		}
+		fmt.Printf("%04d", rowcounter)
+		for _, value := range mylist {
+			if result[value] != "" {
+				if showRTT {
+					fmt.Printf("|%"+width+"s ", result[value])
+				} else {
+					fmt.Printf("|%"+width+"s ", ".")
+				}
+			} else {
+				fmt.Printf("|%"+width+"s ", "!")
+			}
+		}
+		fmt.Println("|")
+		result = make(map[string]string)
+		rowcounter += 1
+	}
+	for {
+		err := p.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
